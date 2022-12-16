6a7c5a6b05cb0c161097d01ad8183af9968caa95
Helga Stock <helga.stock@me.com>
2022-12-16 17:39:09 +0100

    ../setup_s3.sh

---
+++ setup_s3.sh
@@ -0,0 +1,8 @@
+S3_BUCKET_NAME=monitoring-logs
+REGION=us-west-2
+if aws s3 ls "s3://$S3_BUCKET_NAME" 2>&1 | grep -q 'An error occurred'
+then
+    aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+else
+    echo "bucket already exists"
+fi
